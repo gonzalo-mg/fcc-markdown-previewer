872710e51ed5a106a378e5ae45249ac58683b6a2
gon <gonzalomartinezgonzalez@gmail.com>
2023-08-01 13:09:51 +0200

    working version ex tests 3,4

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { marked } from "marked";
+import { defaultEditorText } from "./exampleText";
 
 function App() {
+  const [editorText, setEditorText] = useState(defaultEditorText);
+  const [previewOutput, setpreviewOutput] = useState("previewOutput-0");
+
+  // marked library settings to comply with requirements
+  marked.setOptions({
+    gfm: true,
+    breaks: true,
+  });
+
+  //f to update Previewer
+  const handleEditorChange = () => {
+    // rtranslate text to html with external library
+    const translatedText = marked(editorText, { sanitize: true });
+    // setpreviewOutput
+
+    setpreviewOutput(translatedText);
+  };
+
+  // efect to update Previewer whenever editor is changed; callback avoids state being one step behind
+  useEffect(() => {
+    handleEditorChange();
+  }, [editorText]);
+
+  //useEffect(handleEditorChange, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>GitHub markdown to HTML</header>
+      <main>
+        <article>
+          <h3>Editor</h3>
+          <textarea
+            autoFocus
+            id="editor"
+            name="editor"
+            cols={50}
+            rows={30}
+            value={editorText} // input value forced to be the state variable
+            onChange={(changeEvent) => setEditorText(changeEvent.target.value)} // execute when value is modified by user typing in the textarea
+          ></textarea>
+        </article>
+        <article>
+          <h3>Previewer</h3>
+          <div
+            id="preview"
+            dangerouslySetInnerHTML={{ __html: previewOutput }}
+          ></div>
+        </article>
+      </main>
+      <footer>Gonzalo M.G.</footer>
+    </>
   );
 }
 
---
+++ src/exampleText.js
@@ -0,0 +1,39 @@
+export const defaultEditorText = `# Header Level 1
+
+## Header Level 2
+
+### Header Level 3
+
+Link to FCC: (https://www.freecodecamp.org/learn).
+
+### Code block
+        <html>
+          <head>
+            <title>Test</title>
+          </head>
+
+
+### Code inline
+
+Example line \`<addr>\`.
+
+### List
+
+- First item
+- Second item
+
+### Blockquote
+
+> Homer J. Simpson.
+
+### Image
+
+![Markdown Logo](https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Markdown-mark.svg/208px-Markdown-mark.svg.png "Markdown Logo")
+
+### Bold Text
+
+Regular text **bold text** regulat text.
+
+    
+
+`;
